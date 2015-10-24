07f3e0c37639986802c2e8038f2469d46327832d
Peter deHaan <peter@deseloper.com>
2015-10-24 01:01:22 -0700

    Scaffold: Add eslint & jscs

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ var wrappy = require('wrappy');
 
 var slice = Array.prototype.slice;
 
-function asyncOnce(fn){
-  // our state
+function asyncOnce(fn) {
+  // Our state
   var _err;
   var _result;
   var _called = false;
 
-  function worker(work, done){
+  function worker(work, done) {
     var ctx = work.ctx;
     var initial = work.initial;
     var cb = work.cb;
 
-    function updateState(err, result){
+    function updateState(err, result) {
       _err = err;
       _result = result;
       _called = true;
@@ -24,7 +24,7 @@ function asyncOnce(fn){
       done();
     }
 
-    if(_called){
+    if (_called) {
       cb(_err, _result);
       done();
     } else {
@@ -34,15 +34,15 @@ function asyncOnce(fn){
 
   var _queue = new Queue(worker, 1);
 
-  return function(){
+  return function() {
     var args = slice.call(arguments, 0);
     var work = {
       ctx: this,
       initial: args.slice(0, -1),
-      cb: args.slice(-1)[0]
+      cb: args.slice(-1)[0],
     };
 
-    if(typeof work.cb !== 'function'){
+    if (typeof work.cb !== 'function') {
       throw new Error('async-once only works with node-style async functions');
     }
 
